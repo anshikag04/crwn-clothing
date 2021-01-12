0166130ec470c9b9a5e86d3b4ea8c5b5df78a68b
anshika gupta <tanyagupta391@gmail.com>
2021-01-12 07:07:21 +0530

    homepage

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Homepage from './pages/homepage/homepage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
